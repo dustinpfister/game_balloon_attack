37b8677b467eb08c522e56ad8d6c5116ace632f9
dustinpfister <dustin.pfister@gmail.com>
2017-06-11 17:09:01 -0400

    ship.js

---
+++ js/ship.js
@@ -0,0 +1,36 @@
+var Ship = function (options) {
+
+    this.x = options.x || 0;
+    this.y = options.y || 0;
+    this.w = options.w || 64;
+    this.h = options.h || 32;
+
+    this.maxHP = 3;
+    this.hp = this.maxHP;
+    this.delta = 1;
+    this.attack = false;
+    this.dead = false;
+
+};
+
+// update a ships data
+Ship.prototype.update = function () {
+
+    if (!this.attack) {
+
+        this.x += this.delta;
+
+    } else {
+
+        this.dead = true;
+
+    }
+
+    if (this.x >= Feild.maxW - 32) {
+
+        this.x = Feild.maxW - 32;
+        this.attack = true;
+
+    }
+
+};
