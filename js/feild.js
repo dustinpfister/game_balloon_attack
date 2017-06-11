8255395216099c0ae13f75a85834f875fadb7fe4
dustinpfister <dustin.pfister@gmail.com>
2017-06-11 17:16:43 -0400

    shot.js

--- js/feild.js
+++ js/feild.js
@@ -1,14 +1,6 @@
 var Feild = (function () {
 
-    var boundingBox = function (x1, y1, w1, h1, x2, y2, w2, h2) {
 
-        if ((x1 > x2 + w2) || (x1 + w1 < x2) || (y1 + h1 < y2) || (y1 > y2 + h2)) {
-            return false;
-        } else {
-            return true;
-        };
-
-    };
 
     var api = {
 
@@ -20,63 +12,7 @@ var Feild = (function () {
 
     };
 
-    var Shot = function (options) {
 
-        this.owner = options.owner || 'player';
-        this.age = 0;
-        this.maxAge = 70;
-        this.dead = false;
-        this.size = 5;
-        this.delta = 5; // px per tick
-
-        this.heading = options.heading || Math.PI;
-        this.x = options.x || 0;
-        this.y = options.y || 0;
-
-    };
-
-    Shot.prototype.update = function () {
-
-        var shot = this;
-
-        if (!this.dead) {
-
-            api.ships.forEach(function (ship) {
-
-                if (boundingBox(
-
-                        ship.x, ship.y, ship.w, ship.h,
-                        shot.x, shot.y, shot.size, shot.size)) {
-
-                    ship.hp -= 1;
-
-                    if (ship.hp <= 0) {
-
-                        ship.hp = 0;
-
-                        ship.dead = true;
-
-                    }
-
-                    shot.dead = true;
-
-                }
-
-            })
-
-            this.x += Math.cos(this.heading) * this.delta;
-            this.y += Math.sin(this.heading) * this.delta;
-
-            this.age += 1;
-            if (this.age >= this.maxAge) {
-
-                this.dead = true;
-
-            }
-
-        }
-
-    };
 
     var Gun = function (options) {
 
